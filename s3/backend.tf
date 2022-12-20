69719107bd4801a5c81b2ad0c38a8a1f922e0031
timotomate <75873954+timotomate@users.noreply.github.com>
2022-12-20 16:04:49 +0900

    Update backend.tf

--- s3/backend.tf
+++ s3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" { 
-      bucket         = "inflearn-1031test-apne2-tfstate"
-      key            = "terraform101/s3/terraform.tfstate"
+      bucket         = "timotomate-1031test-apne2-tfstate"
+      key            = "timotomate/s3/terraform.tfstate"
       region         = "ap-northeast-2"  
       encrypt        = true
       dynamodb_table = "terraform-lock"
